e29503587cc3d3f0304f733b60bf5a73020c5dac
Priyam Singh <83497025+priyamsinghh@users.noreply.github.com>
2022-06-29 14:09:23 +0530

    Time: 3 ms (42.47%), Space: 6.2 MB (51.67%) - LeetHub

---
+++ 709-to-lower-case/709-to-lower-case.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for(int i=0;i<(s.size());i++)
+        {
+            if(isupper(s[i]))
+            {
+                s[i]+=32;
+            }
+        }
+        return s;
+    }
+};
