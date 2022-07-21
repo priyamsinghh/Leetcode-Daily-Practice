f7b93765c7fd47b0160d54d4db4dba959c8e9cab
Priyam Singh <83497025+priyamsinghh@users.noreply.github.com>
2022-07-21 22:42:59 +0530

    Time: 82 ms (71.23%), Space: 15.5 MB (74.55%) - LeetHub

---
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
@@ -0,0 +1,10 @@
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if root is None or root.left is None:
+            return root
+        root.left.next = root.right
+        if root.next:
+            root.right.next = root.next.left
+        self.connect(root.left)
+        self.connect(root.right)
+        return root
