ad86020f918b56c3364c6f3ed079c84ace883728
Priyam Singh <83497025+priyamsinghh@users.noreply.github.com>
2023-03-17 18:41:08 +0530

    Added solution - LeetHub

--- Postorder Traversal - GFG/postorder-traversal.cpp
+++ Postorder Traversal - GFG/postorder-traversal.cpp
@@ -124,20 +124,39 @@ struct Node
 }; */
 
 //Function to return a list containing the postorder traversal of the tree.
-void postorder_traversal(Node* root,vector<int>&v)
+vector <int> postOrder(Node* cur)
 {
-    if(root==NULL)
-    {
-        return;
-    }
-    postorder_traversal(root->left,v);
-    postorder_traversal(root->right,v);
-    v.push_back(root->data);
+  
+    vector < int > postOrder;
+    if (cur == NULL) 
+        return postOrder;
 
-}
-vector <int> postOrder(Node* root)
-{
-  vector<int>v;
-  postorder_traversal(root,v);
-  return v;
+    stack < Node * > st;
+    while (cur != NULL || !st.empty()) 
+    {
+        if (cur != NULL) 
+        {
+            st.push(cur);
+            cur = cur -> left;
+        } 
+        else 
+        {
+            Node * temp = st.top() -> right;
+            if (temp == NULL) 
+            {
+                temp = st.top();
+                st.pop();
+                postOrder.push_back(temp -> data);
+                while (!st.empty() && temp == st.top() -> right) 
+                {
+                    temp = st.top();
+                    st.pop();
+                    postOrder.push_back(temp -> data);
+                }
+            } 
+            else 
+                cur = temp;
+        }
+    }
+    return postOrder;
 }
