c3b7302cff81fb2cd84c479458f416f5a226f9a6
Priyam Singh <83497025+priyamsinghh@users.noreply.github.com>
2023-05-04 20:57:31 +0530

    Added solution - LeetHub

--- Shortest Distance in a Binary Maze - GFG/shortest-distance-in-a-binary-maze.cpp
+++ Shortest Distance in a Binary Maze - GFG/shortest-distance-in-a-binary-maze.cpp
@@ -10,19 +10,23 @@ using namespace std;
 
 class Solution {
   public:
-    int shortestPath(vector<vector<int>> &grid, pair<int, int> source,
-                     pair<int, int> destination) {
-        // code here
-        if (source.first == destination.first && source.second == destination.second)
+    int shortestPath(vector<vector<int>> &grid, pair<int, int> source, pair<int, int> destination) 
+    {
+        if (source.first == destination.first &&
+            source.second == destination.second)
             return 0;
-        queue<pair<int,pair<int,int>>>q;
+        queue<pair<int, pair<int,int>>>q;
+        
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>>dist(n,vector<int>(m,1e9));
+        
+        vector<vector<int>> dist(n, vector<int>(m, 1e9));
         dist[source.first][source.second] = 0;
-        q.push({0,{source.first,source.second}});
+        q.push({0, {source.first, source.second}});
+        
         int drow[] = {-1,0,1,0};
         int dcol[] = {0,1,0,-1};
+        
         while(!q.empty())
         {
             auto it = q.front();
@@ -34,18 +38,19 @@ class Solution {
             {
                 int nrow = r + drow[i];
                 int ncol = c + dcol[i];
-                if(nrow<n && ncol<m && nrow >= 0 && ncol >= 0 && 
-                grid[nrow][ncol] == 1 && dis + 1 < dist[nrow][ncol])
+                if(nrow<n && nrow>=0 &&ncol<m &&ncol>=0 && grid[nrow][ncol] == 1 && dis + 1 < dist[nrow][ncol])
                 {
-                    dist[nrow][ncol] = 1 + dis;
-                    if(nrow == destination.first && ncol == destination.second)
-                        return dis + 1;
-                    q.push({dis+1 ,{nrow,ncol}});
+                    dist[nrow][ncol] = dis + 1;
+                    if(nrow == destination.first && destination.second == ncol)
+                    {
+                        return dis+1;
+                    }
+                    q.push({dis+1,{nrow , ncol}});
                 }
             }
-            
         }
         return -1;
+        
     }
 };
 
