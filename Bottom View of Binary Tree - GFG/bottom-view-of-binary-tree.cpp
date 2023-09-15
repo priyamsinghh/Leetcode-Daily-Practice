2614d8fa8945bbd7e4bdae8e621500484f9857ac
Priyam Singh <83497025+priyamsinghh@users.noreply.github.com>
2023-09-16 00:14:05 +0530

    Added solution - LeetHub

--- Bottom View of Binary Tree - GFG/bottom-view-of-binary-tree.cpp
+++ Bottom View of Binary Tree - GFG/bottom-view-of-binary-tree.cpp
@@ -96,33 +96,32 @@ Node* buildTree(string str)
 class Solution {
   public:
     vector <int> bottomView(Node *root) {
-        // Your Code Here
-        vector<int> ans; 
-        if(root == NULL) return ans; 
-        map<int,int> mpp; 
-        queue<pair<Node*, int>> q; 
-        q.push({root, 0}); 
-        while(!q.empty()) {
-            auto it = q.front(); 
-            q.pop(); 
-            Node* node = it.first; 
-            int line = it.second; 
-            mpp[line] = node->data; 
+        // Your Code HereNode 
+        vector<int>ans;
+        queue<pair<Node*, int>>q;  // Node , line
+        q.push({root,0});
+        map<int,int>mpp;
+        while(!q.empty())
+        {
+            auto it = q.front();
+            q.pop();
+            Node *node = it.first;
+            int line = it.second;
+            mpp[line] = node->data;
             
-            if(node->left != NULL) {
-                q.push({node->left, line-1}); 
-            }
-            if(node->right != NULL) {
-                q.push({node->right, line + 1}); 
-            }
-            
+            if(node->left)
+                q.push({node->left, line - 1});
+            if(node->right)
+                q.push({node->right, line + 1});
+                
         }
-        
-        for(auto it : mpp) {
-            ans.push_back(it.second); 
+        for(auto it : mpp)
+        {
+            ans.push_back(it.second);
         }
-        return ans;  
+        return ans;
     }
+    
 };
 
 //{ Driver Code Starts.
